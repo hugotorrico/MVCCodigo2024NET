eb35e8be4ab529d2c02790fa3e3b1d534a1a5752
Hugo Torrico <torricomarquezh@gmail.com>
2024-08-26 22:46:49 -0500

    MVC+Ajax en Course

---
+++ MVCCodigo2024NET/Controllers/CoursesAjaxController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCCodigo2024NET.Models;
+
+namespace MVCCodigo2024NET.Controllers
+{
+    public class CoursesAjaxController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public CoursesAjaxController(SchoolContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var courses = _context.Courses.ToList();
+            return View(courses);
+        }
+
+        public IActionResult CreateCourse()
+        {
+            return PartialView("_CreateCoursePartial");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubmitForm(string name, string description, int credit)
+        {
+            try
+            {
+                var course = new Course
+                {
+                    Name = name,
+                    Description = description,
+                    Credit = credit,
+                    Nuevo=string.Empty
+                };
+                _context.Courses.Add(course);
+                await _context.SaveChangesAsync();
+                return Json(new { message = "Estudiante registrado con éxito." });
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new { message = ex.Message });
+            }
+          
+
+          
+
+           
+        }
+    }
+}
